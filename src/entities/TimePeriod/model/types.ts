58bc7bc28a480645dc1c72a0ee26cdb044ae56ef
Ilia Mashkov <hikkikomori7@gmail.com>
2025-11-19 20:24:19 +0300

    feat: Добавлен слой entities с типами для событий, установлены библиотеки gsap и swiper

---
+++ src/entities/TimePeriod/model/types.ts
@@ -0,0 +1,34 @@
+/**
+ * Entity: TimePeriod
+ * Типы данных для временных периодов и событий
+ */
+
+export interface HistoricalEvent {
+  /**
+   * Год события
+   */
+  readonly year: number
+  /**
+   * Описание события
+   */
+  readonly description: string
+}
+
+export interface TimePeriod {
+  /**
+   * Год начала периода
+   */
+  readonly yearFrom: number
+  /**
+   * Год конца периода
+   */
+  readonly yearTo: number
+  /**
+   * Название категории
+   */
+  readonly label: string
+  /**
+   * События, связанные с этим периодом
+   */
+  readonly events: readonly HistoricalEvent[]
+}
